984c92a06b4b68da1c42253f6bfe04a3497a1ce1
MateusPitura <mateuspitura@gmail.com>
2026-05-01 18:53:35 -0300

    feat: add debug print statements for db_path and basename in BookmarkRepository and get_preferences_path

--- src/repository/BookmarkRepository.py
+++ src/repository/BookmarkRepository.py
@@ -7,6 +7,7 @@ import time
 class BookmarkRepository:
     def __init__(self):
         db_path = f'{get_preferences_path()}/data.db'
+        print(f"🌠 db_path: {db_path}")
         os.makedirs(os.path.dirname(db_path), exist_ok=True)
         self.conn = sqlite3.connect(db_path)
 
--- src/utils/get_preferences_path.py
+++ src/utils/get_preferences_path.py
@@ -2,4 +2,5 @@ import os
 
 def get_preferences_path():
     basename = os.path.basename(os.path.dirname(__file__))
+    print(f"🌠 basename: {basename}")
     return os.path.expanduser(f'~/.config/ulauncher/{basename}')
