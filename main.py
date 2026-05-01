3b63adbfd7c4f0a08f4a872292767621030c908c
MateusPitura <mateuspitura@gmail.com>
2026-05-01 20:13:54 -0300

    refactor: update get_bookmark_items to format URL items and combine with profile items

--- main.py
+++ main.py
@@ -213,17 +213,19 @@ def get_bookmark_items(query="", event=None, extension=None):
 
     max_results = get_max_results(extension)
 
-    items = extension.repository.search_by_full_path(query, max_results)
+    url_items = extension.repository.search_by_full_path(query, max_results)
+
+    url_items_formatted = [append_url(item) for item in url_items]
 
     profile_items = get_profiles_items(event, extension)
 
     # url_items = sort_by_date_last_used(url_items)
 
-    items = profile_items + items
+    items = profile_items + url_items_formatted
     print(f"🌠 items: {items}")
 
     return [
-        ExtensionResultItem(append_url(item, event, extension))
+        ExtensionResultItem(items)
         for item in items
     ]
 
